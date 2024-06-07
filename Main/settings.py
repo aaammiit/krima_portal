afae01aeffcfb94e24a6ae4341775f7097774e13
Amit Kumar Dwivedi <159676340+aaammiit@users.noreply.github.com>
2024-06-07 20:54:19 +0530

    Update settings.py

--- Main/settings.py
+++ Main/settings.py
@@ -75,16 +75,7 @@ WSGI_APPLICATION = 'Main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'krima_json',
-        'USER':'root',
-        'PASSWORD':'',
-        'HOST':'localhost',
-        'PORT':'3306',
-    }
-}
+
 
 
 # Password validation
@@ -93,7 +84,13 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
+    },DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':BASE_DIR / 'db.sqlite3',
+        
+    }
+}
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
