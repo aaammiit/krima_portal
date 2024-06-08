556859300058ffcf36177055f273db3b726b4e36
Amit Kumar Dwivedi <159676340+aaammiit@users.noreply.github.com>
2024-06-08 12:02:28 +0530

    Update settings.py

--- Main/settings.py
+++ Main/settings.py
@@ -74,6 +74,13 @@ WSGI_APPLICATION = 'Main.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':BASE_DIR / 'db.sqlite3',
+        
+    }
+}
 
 
 
